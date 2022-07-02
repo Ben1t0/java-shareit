3eaa4d33a025b4844b2116dc8ff4fbefb036d26a
Georgiy Rusakov <gosha1510@yandex.ru>
2022-07-02 22:45:31 +0300

    fix: remove empty bean usage

--- src/main/java/ru/practicum/shareit/booking/service/BookingServiceImpl.java
+++ src/main/java/ru/practicum/shareit/booking/service/BookingServiceImpl.java
@@ -2,11 +2,8 @@ package ru.practicum.shareit.booking.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import ru.practicum.shareit.booking.storage.BookingRepository;
 
 @Service
 @RequiredArgsConstructor
 public class BookingServiceImpl implements BookingService {
-    private final BookingRepository bookingRepository;
-
 }
--- src/main/java/ru/practicum/shareit/requests/service/ItemRequestServiceImpl.java
+++ src/main/java/ru/practicum/shareit/requests/service/ItemRequestServiceImpl.java
@@ -2,11 +2,8 @@ package ru.practicum.shareit.requests.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import ru.practicum.shareit.requests.storage.ItemRequestRepository;
 
 @Service
 @RequiredArgsConstructor
 public class ItemRequestServiceImpl implements ItemRequestService {
-    private final ItemRequestRepository itemRequestRepository;
-
 }
